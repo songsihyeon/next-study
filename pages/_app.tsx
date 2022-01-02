6c0283500f2cf7bd422e587af3e51bb1a125ea06
songsihyeon <s20040@gsm.hs.kr>
2022-01-02 23:59:12 +0900

    Add QueryClient, QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
