53d6aa78ba0ac1210ff7b7e0fcc4699f2b9919af
songsihyeon <s20040@gsm.hs.kr>
2022-01-06 23:45:25 +0900

    Add lectureList api

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import LectureGroup from "@/components/lecture/lectureGroup";
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
 
       <Menu />
       <Banner />
+      <LectureGroup />
 
       <div title="lectureList">
         <h1>강의 목록</h1>
